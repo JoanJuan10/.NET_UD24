c68aba5c2c8cc1551f37c5ee37d70e3bf532f037
JoanJuan10 <iijoanjuan10@gmail.com>
2021-02-02 11:40:58 +0100

    Ejercicio 1

---
+++ API/API/Models/APIContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models
+{
+    public class APIContext : DbContext
+    {
+        public APIContext(DbContextOptions<APIContext> options) : base (options)
+        {
+
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ API/API/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public int Dni { get; set; }
+        public string Fecha { get; set; }
+    }
+}
