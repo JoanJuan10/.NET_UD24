b6e851205e8a2d88529aef1de34a7c5744725342
JoanJuan10 <iijoanjuan10@gmail.com>
2021-02-02 12:18:34 +0100

    Ejercicio 2

---
+++ API_EX2/API_EX2/Models/Video.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_EX2.Models
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string director { get; set; }
+        public int cli_id { get; set; }
+
+    }
+}
